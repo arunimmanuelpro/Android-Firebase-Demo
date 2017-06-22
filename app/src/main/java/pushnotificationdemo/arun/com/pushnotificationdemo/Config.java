f7d3fba3bba4ccaf0aad5c9dedca3de243f880f4
Arun Jaya Immanuel V <ArunJaya.Immanuel@cognizant.com>
2017-06-22 15:57:02 +0530

    first commit

---
+++ app/src/main/java/pushnotificationdemo/arun/com/pushnotificationdemo/Config.java
@@ -0,0 +1,20 @@
+package pushnotificationdemo.arun.com.pushnotificationdemo;
+
+/**
+ * Created by 560845 on 6/22/2017.
+ */
+
+public class Config {
+    // global topic to receive app wide push notifications
+    public static final String TOPIC_GLOBAL = "global";
+
+    // broadcast receiver intent filters
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String PUSH_NOTIFICATION = "pushNotification";
+
+    // id to handle the notification in the notification tray
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
+    public static final String SHARED_PREF = "ah_firebase";
+}
